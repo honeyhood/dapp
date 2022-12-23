1b7b7b1dd8169700147dea11f6e36c3a3509b95b
honeyh00d <centrcyclona@gmail.com>
2022-12-23 16:52:17 +0300

    Revert "delete.env"
    This reverts commit c196a06b5eeece84860712d6a068056d8c039cfa.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,18 +2,13 @@ import React from 'react';
 
 const Hero = () => {
   return (
-    <div
-      className="hero min-h-screen"
-      style={{ backgroundImage: 'url(/heroImg.jpeg)' }}
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
-      <div className="hero-content text-center text-neutral-content">
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          <h1 className="text-5xl font-bold">Crypto twitter's new home</h1>
+          <p className="py-6">
+            Discuss on-chain data, follow wallet trades and investments with
+            your friends and communities
           </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   daisyui: {
-    themes: ['black'],
+    themes: [
+      {
+        mytheme: {
+          primary: '#111827',
+
+          secondary: '#D926A9',
+
+          accent: '#1FB2A6',
+
+          neutral: '#111827',
+
+          'base-100': '#e0f2fe',
+
+          info: '#facc15',
+
+          success: '#22c55e',
+
+          warning: '#f97316',
+
+          error: '#dc2626',
+        },
+      },
+    ],
   },
   plugins: [require('daisyui')],
 };
